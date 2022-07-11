5beea69857f4e273420737f013ee77329d365c2f
Роман Якименко <romrom2001@mail.ru>
2022-07-11 13:38:30 +0700

    add UserModel

--- src/main/java/com/tangerinedelivery/controllers/UserController.java
+++ src/main/java/com/tangerinedelivery/controllers/UserController.java
@@ -3,6 +3,7 @@ package com.tangerinedelivery.controllers;
 import com.tangerinedelivery.entities.UserEntity;
 import com.tangerinedelivery.exception.EmailAlreadyUsedException;
 import com.tangerinedelivery.exception.UserNotFoundException;
+import com.tangerinedelivery.models.UserModel;
 import com.tangerinedelivery.services.UserService;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,6 +11,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/users")
@@ -23,15 +25,15 @@ public class UserController {
 
 
     @GetMapping("/all")
-    public List<UserEntity> getAll() {
-        return userService.findAll();
+    public List<UserModel> getAll() {
+        return userService.findAll().stream().map(UserModel::ToModel).collect(Collectors.toList());
     }
 
 
     @GetMapping
     public ResponseEntity getUser(@RequestParam Long id) {
         try {
-            return ResponseEntity.ok(userService.findUserById(id));
+            return ResponseEntity.ok(UserModel.ToModel(userService.findUserById(id)));
         } catch (UserNotFoundException e) {
             return ResponseEntity.badRequest().body(e.getMessage());
         }
--- src/main/java/com/tangerinedelivery/models/UserModel.java
+++ src/main/java/com/tangerinedelivery/models/UserModel.java
@@ -1,4 +1,83 @@
 package com.tangerinedelivery.models;
 
+import com.tangerinedelivery.entities.UserEntity;
+
+import javax.persistence.Column;
+
 public class UserModel {
+    private Long id;
+
+    private String email;
+
+    private String firstName;
+
+    private String middleName;
+
+    private String lastName;
+
+    private Boolean emailConfirmed;
+
+    public UserModel() {
+    }
+
+    public static UserModel ToModel(UserEntity user) {
+        UserModel userModel = new UserModel();
+        userModel.setId(user.getUserID());
+        userModel.setEmail(user.getEmail());
+        userModel.setFirstName(user.getFirstName());
+        userModel.setMiddleName(user.getMiddleName());
+        userModel.setLastName(user.getLastName());
+        userModel.setEmailConfirmed(user.getEmailConfirmed());
+        return userModel;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getMiddleName() {
+        return middleName;
+    }
+
+    public void setMiddleName(String middleName) {
+        this.middleName = middleName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public Boolean getEmailConfirmed() {
+        return emailConfirmed;
+    }
+
+    public void setEmailConfirmed(Boolean emailConfirmed) {
+        this.emailConfirmed = emailConfirmed;
+    }
+
+
 }
